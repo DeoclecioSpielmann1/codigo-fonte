e89222f76ab31f070d06b825063b8fdff92c856b
DeocĺecioSpielmann1 <deoclecio62192226@alunos.sc.senac.br>
2026-03-16 21:44:05 -0300

    caraca

---
+++ exercicio2.js
@@ -0,0 +1,23 @@
+início 
+  A ← 8
+  B ← 6
+  média ← (A + B ) / 2
+  se (média >= 7) então
+    escreva "Aprovado"
+  senão
+    escreva "Reprovado"
+  fim-se
+fim
+
+JAVASCRIPT
+
+let A = 8;
+let B = 6;
+
+let media = (A + B) / 2;
+
+if (media >= 7) {
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+}
---
+++ exercicio3.js
@@ -0,0 +1,15 @@
+início
+  para numero ← 1 até 20 faça
+    se (numero % 2 = 0) então
+      escreva numero
+    fim-se
+  fim-para
+fim
+
+JAVASCRIPT
+
+for (let numero = 1; numero <= 20; numero++) {
+  if (numero % 2 === 0) {
+    console.log(numero);
+  }
+}
